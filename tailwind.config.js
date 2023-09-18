da62bc129439d7c91bfdd3a753804073566d61dc
razj <josefraz@seznam.cz>
2023-09-18 20:00:57 +0200

    Flowbite.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media',    // set dark/light mode automatically
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"   // flowbite js
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')    // flowvite plugin
+  ],
 }
 
