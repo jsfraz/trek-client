c15fdf3925b43dac3946ae50a0913679e6e6b80a
razj <josefraz@seznam.cz>
2023-09-18 20:19:41 +0200

    Light mode.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, Inject } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,26 @@ import { initFlowbite } from 'flowbite';
 export class AppComponent implements OnInit {
   title = 'trek-client';
 
+  isDarkMode: boolean = false;
+
+  constructor(
+    private renderer: Renderer2,
+    private elementRef: ElementRef,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
   ngOnInit(): void {
     // init flowbite
     initFlowbite();
+  }
+
+  toggleDarkMode() {
+    // Toggle the "dark" class on the body element
+    if (this.isDarkMode) {
+      this.renderer.removeClass(this.document.body, 'dark');
+    } else {
+      this.renderer.addClass(this.document.body, 'dark');
+    }
+    this.isDarkMode = !this.isDarkMode; // Toggle the mode
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'media',    // set dark/light mode automatically
+  darkMode: 'class',    // set dark/light mode automatically
   content: [
     "./src/**/*.{html,ts}",
     "./node_modules/flowbite/**/*.js"   // flowbite js
